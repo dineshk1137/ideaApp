1ccdfcc3787eaab662b8ec2e7c789209a20b4ad6
dineshk1137 <chandradinesh938@gmail.com>
2023-07-13 11:29:05 +0530

    downloaded mongoDB

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+
+    userId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    },
+
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        minLength : 10,
+        lowercase : true
+    }, 
+    userType : {
+        type : String,
+        required : true,
+        default : "CUSTOMER",
+        enum : ["CUSTOMER","ADMIN"]
+    }
+},{timestamps : true});
+
+
+/**
+ * define the collection where it willbe stored 
+ * 
+*/
+module.exports = mongoose.model("User",userSchema);
--- server.js
+++ server.js
@@ -1,6 +1,48 @@
-const express = require("express")
-const serverConfig = require("./configs/server.config")
+const express = require("express");
+const serverConfig = require("./configs/server.config");
+const mongoose = require("mongoose");
+const dbConfig = require("./configs/db.config");
+const userModel = require("./models/user.model");
 const app = express();
+/**
+ * logic to connect to MongoDB and create an ADMIN user
+ */
+mongoose.connect(dbConfig.DB_URL);
+
+const db = mongoose.connection;
+
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("database is connected");
+
+    init();
+})
+
+async function init(){
+    
+    let admin = await userModel.findOne({
+        userId : "admin"
+    }) 
+    if(admin){
+        console.log("Admin already present");
+        return;
+    }
+
+    admin = await userModel.create({
+        name : "Dinesh Kumar", 
+        userId : "admin",
+        email : "chandradinesh938@gmail.com",
+        userType: "ADMIN",
+        password : "Dinesh@1"
+    });
+    console.log(admin);
+}
+
+
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server Started on port no ${serverConfig.PORT}`);
